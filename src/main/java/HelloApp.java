9581d03f57ed2802f33b1b16374b52005c36245b
kk4986 <kk4986@srmist.edu.in>
2026-03-18 11:16:17 +0530

    Updated UC5 code with improved enhanced loop logic

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String names;
+        StringBuilder sb = new StringBuilder();
 
-        if (args.length > 0) {
-            names = String.join(", ", args);
+        if (args.length == 0) {
+            sb.append("World");
         } else {
-            names = "World";
+            int i = 0;
+            for (String name : args) {
+                sb.append(name);
+                if (i < args.length - 1) {
+                    sb.append(", ");
+                }
+                i++;
+            }
         }
 
-        System.out.println("Hello, " + names + "!");
+        System.out.println("Hello, " + sb.toString() + "!");
     }
 }
